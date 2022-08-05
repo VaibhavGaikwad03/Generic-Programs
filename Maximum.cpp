71cdcc98506777a7a7f798291a94d0fd74700c0f
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-05 10:56:45 +0530

    Add files via upload

---
+++ Maximum.cpp
@@ -0,0 +1,36 @@
+//Write a generic program to accept N numbers from user and return largest value.
+
+#include<iostream>
+using namespace std;
+
+template<class T>
+
+T Maximum(T Arr[], int iSize)
+{
+    int i = 0;
+    T Max = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(Max < Arr[i])
+        {
+            Max = Arr[i];
+        }
+    }
+    return Max;
+}
+
+int main()
+{
+    int iArr[5] = {11, 21, 51, 101, 111};
+
+    int iRet = Maximum(iArr, 5);
+    cout<<"Maximum number is : "<<iRet<<endl;
+
+    float fArr[5] = {11.12, 21.45, 51.47, 101.16, 75.26};
+
+    float fRet = Maximum(fArr, 5);
+    cout<<"Maximum number is : "<<fRet<<endl;
+
+    return 0;
+}
